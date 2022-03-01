66b3f611e9dbb88a9b6a0f7b2b824c8ce38717dd
ClaudiaThalita <claudiathalitamendes@outlook.com>
2022-02-28 23:07:14 -0300

    feature: Aulade Repository e Service

---
+++ Projeto-01/src/main/java/com/claudiathalita/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.claudiathalita.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.claudiathalita.domain.Categoria;
+import com.claudiathalita.repositories.CategoriaRepository;
+
+@Service 
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}
