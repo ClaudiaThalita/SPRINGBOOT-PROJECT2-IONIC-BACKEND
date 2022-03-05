4caaa6ec2d931d397a79af0ca5d301a012dce481
ClaudiaThalita <claudiathalitamendes@outlook.com>
2022-03-04 22:39:16 -0300

    criando uma operaçao de instaciaçao

--- Projeto-01/src/main/java/com/claudiathalita/Projeto01Application.java
+++ Projeto-01/src/main/java/com/claudiathalita/Projeto01Application.java
@@ -1,13 +1,33 @@
 package com.claudiathalita;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.claudiathalita.domain.Categoria;
+import com.claudiathalita.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Projeto01Application {
+public class Projeto01Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
