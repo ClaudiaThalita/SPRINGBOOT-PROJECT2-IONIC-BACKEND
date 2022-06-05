c6193800f87114e138d0111c5235ab01dc27a28d
ClaudiaThalita <claudiathalitamendes@outlook.com>
2022-06-05 20:16:26 -0300

    Aula  47- Validação customizada: email não repetido na atualização de cliente

--- Projeto-01/src/main/java/com/claudiathalita/dto/ClienteDTO.java
+++ Projeto-01/src/main/java/com/claudiathalita/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.claudiathalita.domain.Cliente;
+import com.claudiathalita.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
